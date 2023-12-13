616f44762c3638ecb60f45485a8f27de7230e168
tolseone <asemisynov@mail.ru>
2023-12-14 00:08:04 +0300

    model_item.go - add struct Items

--- go/model_item.go
+++ go/model_item.go
@@ -9,12 +9,12 @@
 package swagger
 
 type Item struct {
-
-	ItemId string `json:"item_id"`
-
-	Name string `json:"name"`
-	// Редкость предмета
-	Rarity string `json:"rarity"`
-	// Описание предмета
+	ItemId      string `json:"item_id"`
+	Name        string `json:"name"`
+	Rarity      string `json:"rarity"`
 	Description string `json:"description,omitempty"`
+}
+
+type Items struct {
+	Items []Item `json:"items"`
 }
