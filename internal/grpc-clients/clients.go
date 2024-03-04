91a521f2710d8b13c8bfbed6ed928d253695e7cc
tolseone <asemisynov@mail.ru>
2024-03-04 08:48:53 +0300

    clients.go - created new package to implement grpc clients (itemService)

---
+++ internal/grpc-clients/clients.go
@@ -0,0 +1,20 @@
+package clients
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+
+	itemgrpc "go-server/internal/clients/item/grpc"
+	"go-server/internal/config"
+)
+
+func NewItemClient(ctx context.Context, cfg *config.Config) (*itemgrpc.Client, error) {
+	loggerSlog := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	itemClient, err := itemgrpc.New(ctx, loggerSlog, cfg.Clients.Item.Address, cfg.Clients.Item.Timeout, cfg.Clients.Item.RetriesCount)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create item client: %w", err)
+	}
+	return itemClient, nil
+}
