8cbed7e83f23703c9884ed95f4f446d0877bbf5b
tolseone <asemisynov@mail.ru>
2024-03-04 08:52:35 +0300

    handlerItem.go - added new field itemServiceClient to ItemHandlerStruct. All funcs of ItemServiceClient used into handler routes

--- internal/controllers/handlers/api-service/handlerItem.go
+++ internal/controllers/handlers/api-service/handlerItem.go
@@ -1,41 +1,53 @@
 package handlerapi
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/google/uuid"
 	"github.com/julienschmidt/httprouter"
 
+	itemgrpc "go-server/internal/clients/item/grpc"
+	"go-server/internal/config"
+	"go-server/internal/grpc-clients"
 	"go-server/internal/models"
 	"go-server/pkg/logging"
 
 )
 
 type ItemHandler struct {
-	logger    *logging.Logger
-	validator *validator.Validate
+	logger            *logging.Logger
+	validator         *validator.Validate
+	itemServiceClient *itemgrpc.Client
 }
 
 func NewItemHandler() *ItemHandler {
+	config := config.GetConfig()
+	itemServiceClient, err := clients.NewItemClient(context.Background(), config)
+	if err != nil {
+		panic("failed to create item client: " + err.Error())
+	}
 	return &ItemHandler{
-		logger:    logging.GetLogger(),
-		validator: validator.New(),
+		logger:            logging.GetLogger(),
+		validator:         validator.New(),
+		itemServiceClient: itemServiceClient,
 	}
 }
 
 func (h *ItemHandler) GetItemList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	items, err := model.LoadItems()
+	const op = "handlerapi.GetItemList"
+
+	items, err := h.itemServiceClient.GetAllItems(context.TODO())
 	if err != nil {
-		h.logger.Errorf("failed to get items: %v", err)
+		h.logger.Errorf("failed to get items: %s: %s", op, err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
 	itemJSON, err := json.Marshal(items)
 	if err != nil {
-		h.logger.Errorf("ошибка при преобразовании пользователей в JSON: %v", err)
+		h.logger.Errorf("ошибка при преобразовании пользователей в JSON: %s: %s", op, err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
@@ -48,7 +60,7 @@ func (h *ItemHandler) GetItemList(w http.ResponseWriter, r *http.Request, params
 func (h *ItemHandler) GetItemByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	itemID := params.ByName("uuid")
 
-	item, err := model.LoadItem(itemID)
+	item, err := h.itemServiceClient.GetItem(context.TODO(), itemID)
 	if err != nil {
 		h.logger.Errorf("failed to get item by UUID: %v", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -78,13 +90,14 @@ func (h *ItemHandler) CreateItem(w http.ResponseWriter, r *http.Request, params 
 		return
 	}
 
-	id, err := newItem.Save()
+	id, err := h.itemServiceClient.CreateItem(context.TODO(), newItem.Name, newItem.Rarity, newItem.Description)
 	if err != nil {
 		h.logger.Fatalf("failed to create item: %v", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	newItem.ItemId = id.(uuid.UUID)
+
+	newItem.ItemId = id
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
@@ -94,7 +107,8 @@ func (h *ItemHandler) CreateItem(w http.ResponseWriter, r *http.Request, params 
 func (h *ItemHandler) DeleteItemByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	itemID := params.ByName("uuid")
 
-	if err := model.DeleteItem(itemID); err != nil {
+	_, err := h.itemServiceClient.DeleteItem(context.TODO(), itemID)
+	if err != nil {
 		h.logger.Errorf("failed to delete item by UUID: %v", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
